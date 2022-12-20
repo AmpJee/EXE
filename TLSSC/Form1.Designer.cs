9c2a74868c36a43e87261802fedb0da853d225eb
AmpJee <83893165+AmpJee@users.noreply.github.com>
2022-12-20 18:26:36 +0700

    all file here

---
+++ TLSSC/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace TLSSC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.Color.MistyRose;
+            this.chart1.BorderlineColor = System.Drawing.Color.DarkCyan;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Enabled = false;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(113, 56);
+            this.chart1.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.SeaGreen;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "R";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(925, 491);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AllowDrop = true;
+            this.comboBox1.Font = new System.Drawing.Font("TH Krub", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(16, 41);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(93, 36);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.button1.ForeColor = System.Drawing.Color.Transparent;
+            this.button1.Location = new System.Drawing.Point(16, 95);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 52);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AllowDrop = true;
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("TH Krub", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label1.Location = new System.Drawing.Point(27, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 28);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "COM";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(16, 155);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(93, 52);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "STOP";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("TH Krub", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(400, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(381, 56);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Thalassemia Screening";
+            this.label2.Click += new System.EventHandler(this.label2_Click_1);
+            // 
+            // btnExport
+            // 
+            this.btnExport.BackColor = System.Drawing.Color.Crimson;
+            this.btnExport.ForeColor = System.Drawing.Color.White;
+            this.btnExport.Location = new System.Drawing.Point(16, 419);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(89, 57);
+            this.btnExport.TabIndex = 9;
+            this.btnExport.Text = "Save";
+            this.btnExport.UseVisualStyleBackColor = false;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("TH Krub", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(36, 327);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 40);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "100";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Orange;
+            this.button5.ForeColor = System.Drawing.Color.White;
+            this.button5.Location = new System.Drawing.Point(16, 214);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(93, 46);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "RELOAD";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.ClientSize = new System.Drawing.Size(1067, 558);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.chart1);
+            this.Font = new System.Drawing.Font("TH Krub", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Thalassemia Screening";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button5;
+        public System.Windows.Forms.Button btnExport;
+    }
+}
+
---
+++ TLSSC/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+using System.IO;
+using Microsoft.VisualBasic.FileIO;
+
+namespace TLSSC
+{
+    public partial class Form1 : Form
+    {
+
+        SerialPort port;
+        string getvalue;
+        int[] data = new int[50000];
+        int countdata = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            foreach(string port in ports)
+            {
+                comboBox1.Items.Add(port);
+            }
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread mastertread;
+            mastertread = new Thread(runit);
+            mastertread.Start();
+            port = new SerialPort(comboBox1.Text, 9600);
+            port.Open();
+        //    countdata = 0;
+        }
+
+        void runit()
+        {
+            while (true)
+            {
+                try
+                {
+                    
+                    if (port.IsOpen == true)
+                    {
+                        int i = 0;
+                        while ( i<=50000)
+                        {
+                            getvalue = port.ReadLine();
+                            data[i] = Convert.ToInt32(getvalue);
+                            countdata++;
+                            chart1.Invoke((MethodInvoker)(() => chart1.Series["R"].Points.AddXY(Convert.ToInt32(i), Convert.ToInt32(getvalue))));
+                            i++;
+                        }
+                        
+                    }
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine("{0} Exception caught.", ex);
+                }
+            }
+        }
+        
+        string path = @"C:\Users\user\Desktop\PROJECT\testing\test.csv";
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //var file = 
+            
+      /*      countdata = 0;
+            for (int j = 0; j < 100; j++)
+            {
+                data[j] = 150 - j;
+                countdata++;
+            }*/
+            // Set the variable "delimiter" to ", ".
+            
+            string delimiter = ", ";
+
+            for (int i = 0; i < countdata; i++)
+            {            
+                // This text is added only once to the file.
+                if (!File.Exists(path))
+                {
+                    // Create a file to write to.
+                    string createText = "Time" + delimiter + "RGB" + Environment.NewLine;
+                    File.WriteAllText(path, createText);
+                }
+
+                // This text is always added, making the file longer over time
+                // if it is not deleted.
+                string appendText = (i+1) + "," + data[i] + Environment.NewLine;
+                File.AppendAllText(path, appendText);
+                
+            }
+            
+            // add(data, k, "file.csv");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            port.Close();
+        }
+       
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // restart the program
+            Application.Restart();
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            /*
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.InitialDirectory = @"C:\\";      
+            saveFileDialog1.Title = "Save text Files";
+            saveFileDialog1.CheckFileExists = true;
+            saveFileDialog1.CheckPathExists = true;
+            saveFileDialog1.DefaultExt = "txt";
+            saveFileDialog1.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            
+            {
+                string name = saveFileDialog1.FileName;
+                string delimiter = ", ";
+
+                /*for (int i = 0; i < countdata; i++)
+                {
+                    // This text is added only once to the file.
+                    if (!File.Exists(name))
+                    {
+                        // Create a file to write to.
+                        string createText = "Time" + delimiter + "RGB" + Environment.NewLine;
+                        File.WriteAllText(name, createText);
+                    }
+
+                    // This text is always added, making the file longer over time
+                    // if it is not deleted.
+                    string appendText = (i + 1) + "," + data[i] + Environment.NewLine;
+                    File.AppendAllText(name, appendText);
+
+                }
+            }
+            */
+
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "CSV|*.csv";
+            saveFileDialog1.Title = "Save an .csv file";
+            saveFileDialog1.ShowDialog();
+
+            // If the file name is not an empty string open it for saving.
+            if (saveFileDialog1.FileName != "")
+            {
+                // Saves the Image via a FileStream created by the OpenFile method.
+                // Saves the Image in the appropriate ImageFormat based upon the
+                // File type selected in the dialog box.
+                // NOTE that the FilterIndex property is one-based.
+                string delimiter = ", ";
+
+                for (int i = 0; i < countdata; i++)
+                {
+                    // This text is added only once to the file.
+
+                    if (!File.Exists(saveFileDialog1.FileName))
+                    {
+                        // Create a file to write to.
+                        string createText = "Time" + delimiter + "RGB" + Environment.NewLine;
+                        File.WriteAllText(saveFileDialog1.FileName, createText);
+                    }
+
+
+                    // This text is always added, making the file longer over time
+                    // if it is not deleted.
+                    string appendText = (i + 1) + "," + data[i] + Environment.NewLine;
+                    File.AppendAllText(saveFileDialog1.FileName, appendText);
+
+                }
+
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label3.Text = getvalue;
+        }
+
+        private void button5_Click_1(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
